b1cb91c7d23c5c0ed3b4957cce0a51fb81703276
Shmuela <shmuela@500tech.com>
2016-11-29 12:46:02 +0200

    add table component

---
+++ src/app/table/table.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  template: `
+    <p>
+      table Works!
+    </p>
+  `,
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
