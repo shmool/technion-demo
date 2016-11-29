f7336f6d4fdb1d80bcf708a24f0801245b2d8376
Shmuela <shmuela@500tech.com>
2016-11-29 13:26:23 +0200

    input and output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+    <app-table [list]="todoList" 
+               (itemClick)="onItemClick($event)"></app-table>
     <h1>
       Hello! {{ opmName }} {{ title }} 
     </h1>
@@ -10,6 +12,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  onItemClick(e) {
+    console.log('hello from app', e);
+  }
+
   title = 'App works!';
-  opmName = 'OPM Rocks!'
+  opmName = 'OPM Rocks!';
+  todoList = [
+    {name: 'Hello'},
+    {name: 'World'},
+    {name: 'OPM'},
+    {name: 'It works!'},
+  ];
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-table',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
     <p>
       table Works!
     </p>
+    
+    <ul>
+      <li *ngFor="let item of list" (click)="onClick($event)">{{ item.name }}</li>
+    </ul>
   `,
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+
+  onClick(e) {
+    console.log('hello', e);
+    this.itemClick.emit({type: 'click', originalEvent: e});
+  }
+
+@Input() list;
+  @Output() itemClick:EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
